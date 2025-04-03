9c64613208ffd89304bbf55794809d11dd7061a4
Dori <qarkchanjin@gmail.com>
2025-04-03 15:32:14 +0900

    Add Template Art Resource
    템플릿 리소스 추가

--- Project_DL/Intermediate/Build/BuildRulesProjects/Project_DLModuleRules/obj/Development/Project_DLModuleRules.AssemblyInfo.cs
+++ Project_DL/Intermediate/Build/BuildRulesProjects/Project_DLModuleRules/obj/Development/Project_DLModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project_DLModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e626fe39fd06da85bd3a663a3ee7a11a767b25c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8e5b4c71f6fde8308a4300895a573655e9d9edc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project_DLModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project_DLModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
